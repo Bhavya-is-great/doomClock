db5bf3d628fe588260f30c483642c54ca0734d9d
Bhavya-is-great <bdisop1234@gmail.com>
2026-02-19 21:37:21 +0530

    Font face made

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const clock = document.getElementById("clock");
 
-const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
 const size = 280;
 const cx = size / 2;
 const cy = size / 2;
-const radius = 115;
+
+const numberRadius = 105;
+const dotRadius = 132;
+
+const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 hours.forEach(hour => {
-
     const degrees = (hour % 12) * 30;
     const radians = degrees * Math.PI / 180;
 
-    const x = cx + radius * Math.sin(radians);
-    const y = cy - radius * Math.cos(radians);
+    const x = cx + numberRadius * Math.sin(radians);
+    const y = cy - numberRadius * Math.cos(radians);
 
     const el = document.createElement("div");
     el.className = "hour";
@@ -24,3 +25,26 @@ hours.forEach(hour => {
 
     clock.appendChild(el);
 });
+
+const dotCount = 60;
+
+for (let i = 0; i < dotCount; i++) {
+    const degrees = i * (360 / dotCount);
+    const radians = degrees * Math.PI / 180;
+
+    const x = cx + dotRadius * Math.sin(radians);
+    const y = cy - dotRadius * Math.cos(radians);
+
+    const dot = document.createElement("div");
+    dot.className = "dot";
+
+    dot.style.left = `${x}px`;
+    dot.style.top = `${y}px`;
+
+    if (i % 5 === 0) {
+        dot.style.width = "6px";
+        dot.style.height = "6px";
+    }
+
+    clock.appendChild(dot);
+}
